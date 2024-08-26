cc9e6773f9decc8a228b46cc3f3c3025bbd4cc1a
shigenogoro <kyle990987@gmail.com>
2024-08-26 16:30:04 -0400

    Home Page Skeleton

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,39 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
 
   return (
       <div>
-        <h1>Home Page</h1>
+        <header>
+          <Navbar />
+        </header>
+
+        <main>
+          <div className="background-image-container"></div>
+          <div className="main-container">
+            <div className="intro-container">
+              <h2>Hi there!</h2>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima esse voluptate illo eius itaque eaque enim, sapiente minus? Officiis omnis perferendis praesentium repellat rem repudiandae expedita magni reprehenderit nihil voluptatum cumque, eos repellendus minima fugiat tempore fuga dolores minus quasi, non atque id saepe molestiae enim! Nulla optio nobis itaque.
+              </p>
+
+              <br />
+              
+              <h3>Currently, I'm: </h3>
+
+              <ul>
+                <li>Student @UMass</li>
+                <li>Internship</li>
+              </ul>
+            </div>
+
+            <div className="links-container">
+              <h2>Links</h2>
+            </div>
+          </div>
+        </main>
+        
       </div>
   )
 }
---
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar navbar-fixed-top">
+            <ul id="nav-list">
+                <li className="nav-item">Home</li>
+                <li className="nav-item">Resume</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
