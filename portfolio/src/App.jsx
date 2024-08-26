f54c3d055ef2ca9b57c8ada5cf76888c0f972702
shigenogoro <kyle990987@gmail.com>
2024-08-26 17:11:24 -0400

    Home Page Content Skeleton

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
               <br />
               
-              <h3>Currently, I'm: </h3>
+              <h3>Currently, I am: </h3>
 
               <ul>
                 <li>Student @UMass</li>
@@ -30,6 +30,40 @@ const App = () => {
 
             <div className="links-container">
               <h2>Links</h2>
+              <div className="social-container">
+                {/* GitHub Page */}
+                <a 
+                  href="https://github.com/shigenogoro"
+                  className="fa fa-github social-icon"
+                ></a>
+
+                {/* LinkedIn Page */}
+                <a 
+                  href="https://www.linkedin.com/in/sheng-kai-wen-a90602130/"
+                  className='fa fa-linkedin social-icon'
+                ></a>
+
+                {/* Instagram Page */}
+                <a 
+                  href="https://www.instagram.com/kyle990987/"
+                  className='fa fa-instagram social-icon'
+                ></a>
+
+                {/* Facebook Page */}
+                <a 
+                  href="https://www.facebook.com/kyle.wen.12"
+                  className='fa fa-facebook social-icon'  
+                ></a>
+              </div>
+
+              <div className="email-container">
+                <a
+                  href="mailto:gorowen56@gmail.com"
+                >
+                  <i className="fa fa-envelope email-icon"></i>
+                  gorowen56@gmail.com
+                </a>
+              </div>
             </div>
           </div>
         </main>
