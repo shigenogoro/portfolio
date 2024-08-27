2c244be62d57de4b003d2a1d144925b01a7facb6
shigenogoro <kyle990987@gmail.com>
2024-08-26 22:45:18 -0400

    Typewriter Title

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,26 +1,51 @@
+import Typewriter from 'typewriter-effect'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Intro from './components/Intro/Intro'
 import Links from './components/Links/Links'
 
 const App = () => {
+return (
+        <div>
+            <header>
+                <Navbar />
+            </header>
 
-  return (
-      <div>
-        <header>
-          <Navbar />
-        </header>
+            <main>
+                <div className="background-image-container">
+                    <div className="title-container">
+                          <h1 id="title">
+                              Hello, I am
+                              <br />
+                              <Typewriter 
+                                options={{
+                                    strings: [
+                                        'Sheng-Kai Wen',
+                                        'an engineer',
+                                        'a hockey player and baseball player'
+                                    ],
+                                    autoStart: true,
+                                    loop: true,
+                                    pauseFor: 100
+                                }}
+                              />
+                          </h1>
+                    </div>
+                </div>
 
-        <main>
-          <div className="background-image-container"></div>
-          <div className="main-container">
-            <Intro />
-            <Links />
-          </div>
-        </main>
-        
-      </div>
-  )
+
+                <div className="main-container">
+                    <div id="main-intro">
+                        <Intro />
+                    </div>
+
+                    <div id="main-links">
+                        <Links />
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
 }
 
 export default App
